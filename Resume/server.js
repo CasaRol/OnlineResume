796db58c98343fa80188b7bee5c8ccbafa4e19ca
CasaRol <alexander-rol@hotmail.com>
2021-01-06 19:17:32 +0100

    Added a few modules and prepared for github login

--- Resume/server.js
+++ Resume/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+require('dotenv').config({ path: __dirname + '/.env' });
 const fs = require('fs');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { env } = require('process');
 const server = express();
 
 server.use(bodyParser.urlencoded({ extended: false }));
@@ -19,11 +21,20 @@ server.route("/").get((req, res) => {
 
 });
 
-server.get('/ping', (req, res) => {
+server.get("/ping", (req, res) => {
     console.log("ping caught");
     res.sendStatus(200);
     res.end();
-})
+});
+
+const client_id = process.env.GITHUB_CLIENT_ID;
+const client_secret = process.env.GITHUB_CLIENT_SECRET;
+
+server.get("/login/github", (req, res) => {
+    const url = 'https://github.com/login/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=http://casarol.site/login/github/callback';
+    res.redirect(url);
+});
+server.get("/login/github/callback", (req, res) => {});
 
 let port = 8080
 server.listen(port);
